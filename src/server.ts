6e9af6922d5addce551b6391fc2f01961217c428
BryanDietrichBernhardt <bryandbernhardt@gmail.com>
2023-01-17 03:42:34 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.register(cors);
+
+app.get("/", async () => {
+  const habits = await prisma.habit.findMany({
+    orderBy: {
+      created_at: "asc",
+    },
+  });
+
+  return habits;
+});
+
+const port = 3333;
+app
+  .listen({
+    port: port,
+  })
+  .then(() => {
+    console.log(`Server running on port ${port}`);
+  });
